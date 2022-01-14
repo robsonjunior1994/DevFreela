d051c9a4f482109744b0241e043257ba8cbaa10f
robsonjunior1994 <robsonjunior1994@gail.com>
2022-01-14 09:07:29 -0300

    Primeiro commit. Iniciamos o projeto falando sobre os controllers e as actions.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
+
