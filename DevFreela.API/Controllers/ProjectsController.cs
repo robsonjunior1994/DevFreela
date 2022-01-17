a9587b8346c9aa4d51a31225672bf31b49dd12a7
robsonjunior1994 <robsonjunior1994@gail.com>
2022-01-17 09:48:39 -0300

    Implementando os endpoints do projeto inicial. 11% do curso.

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -71,6 +71,26 @@ namespace DevFreela.API.Controllers
             return BadRequest();
         }
 
+        // api/projects/1/comments POST
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createCommentModel)
+        {
+            return NoContent();
+        }
+        // api/projects/1/start PUT
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        // api/projects/1/finish
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+
     }
 }
 
